fbcf6d9886118e60b084bd0aa502c40f39ed0b16
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-01-04 15:32:27 +0100

    updates promotion serializer

--- app/serializers/spree/wombat/promotion_serializer.rb
+++ app/serializers/spree/wombat/promotion_serializer.rb
@@ -3,7 +3,9 @@ require 'active_model_serializers'
 module Spree
   module Wombat
     class PromotionSerializer < ActiveModel::Serializer
-      attributes :name, :code, :category_name, :category_code
+      attributes :name, :category_name, :category_code
+
+      has_many :codes, serializer: Spree::Wombat::PromotionCodeSerializer
 
       def category_name
         object.promotion_category.try(:name)
