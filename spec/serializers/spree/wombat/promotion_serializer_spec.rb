e6fd8f209b1c199981b91440f48b779bcd918013
Alessio Rocco <alessio.rocco.lt@gmail.com>
2016-01-04 15:32:27 +0100

    updates promotion serializer

--- spec/serializers/spree/wombat/promotion_serializer_spec.rb
+++ spec/serializers/spree/wombat/promotion_serializer_spec.rb
@@ -3,12 +3,16 @@ require 'spec_helper'
 module Spree
   module Wombat
     describe PromotionSerializer, type: :serializer do
-      let(:promotion) { Spree::Promotion.new(name: "Promo", code: "pro") }
+      let(:promotion_code) { Spree::PromotionCode.new( value: 'abc' ) }
+      let(:promotion) { Spree::Promotion.new(name: "Promo", codes: [promotion_code]) }
       let(:serialized) { JSON.parse(PromotionSerializer.new(promotion, root: false).to_json) }
+      let(:serialized_promotion_code) do
+        JSON.parse(PromotionCodeSerializer.new(promotion_code, root: false).to_json)
+      end
 
       before { promotion.build_promotion_category(name: "Category", code: "cat") }
 
-      it { expect(serialized['code']).to eq 'pro' }
+      it { expect(serialized['codes']).to eq([serialized_promotion_code]) }
       it { expect(serialized['name']).to eq 'Promo' }
       it { expect(serialized['category_code']).to eq 'cat' }
       it { expect(serialized['category_name']).to eq 'Category' }
